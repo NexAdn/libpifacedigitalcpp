1470504382063dc611bb99c3d3c1cde7d8f13004
NexAdn <nexadn@yandex.com>
2018-07-08 14:04:50 +0200

    Implement pf::PiFace

--- src/PiFace.cpp
+++ src/PiFace.cpp
@@ -1,1 +1,47 @@
 #include "PiFace.hpp"
+
+#include "pifacedigital.h"
+
+namespace pf
+{
+PiFace::PiFace(uint8_t hardwareAddress) : hardwareAddress(hardwareAddress)
+{
+    this->spiFileDescriptor = pifacedigital_open(hardwareAddress);
+}
+
+PiFace::~PiFace()
+{
+    pifacedigital_close(this->hardwareAddress);
+}
+
+void PiFace::reset()
+{
+    this->bulkSetOutput(0x00);
+}
+
+void PiFace::setOutput(uint8_t outputNo, bool value)
+{
+    std::lock_guard<std::mutex> lock(this->pfdOutputMutex);
+    pifacedigital_write_bit(static_cast<uint8_t>(value), outputNo, OUTPUT, this->hardwareAddress);
+}
+
+void PiFace::bulkSetOutput(uint8_t outputBits)
+{
+    for (uint8_t i = 0; i < 8; i++)
+        this->setOutput(i, outputBits & (0x1 << i));
+}
+
+bool PiFace::getInput(uint8_t inputNo) const
+{
+    return static_cast<bool>(pifacedigital_read_bit(inputNo, INPUT, this->hardwareAddress));
+}
+
+uint8_t PiFace::bulkGetInput() const
+{
+    uint8_t res{0x00};
+    for (uint8_t i = 0; i < 8; i++)
+        res |= this->getInput(i) << i;
+
+    return res;
+}
+} // namespace pf
--- src/PiFace.hpp
+++ src/PiFace.hpp
@@ -1,3 +1,37 @@
 #pragma once
 
-#include <iostream>
+#include <stdint.h>
+
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+namespace pf
+{
+class PiFace
+{
+public:
+    PiFace(uint8_t hardwareAddress = 0x00);
+    ~PiFace();
+
+    void reset();
+
+    void setOutput(uint8_t outputNo, bool value);
+    void bulkSetOutput(uint8_t outputBits);
+
+    bool getInput(uint8_t inputNo) const;
+    uint8_t bulkGetInput() const;
+
+    void registerInputStateChangeNotifier(std::condition_variable cv);
+
+private:
+    int spiFileDescriptor{-1};
+    uint8_t hardwareAddress{0x00};
+
+    std::mutex pfdOutputMutex;
+
+    std::thread inputListener;
+    std::vector<std::condition_variable> inputStateChangeNotifiers;
+};
+} // namespace pf
